886a65db84c15cd1d0b1eef145d8e50db0a9925b
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-06-09 13:07:14 +0500

    add global app config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const allowedOrigins = ['http://localhost:3000'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: allowedOrigins,
+  });
+
+   /**
+   * Setting the global prefix api/endpoint
+   */
+  app.setGlobalPrefix('api');
+  /**
+   * Using the DTO pipe for request validation
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
