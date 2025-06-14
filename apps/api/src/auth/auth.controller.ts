b290d8df5ab4c56515fe59ed58e6407a36b51e02
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-06-14 14:05:26 +0500

    feat: mark the login and create user route as public

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { AuthService } from './providers/auth.service';
+import { Public } from './decrators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
+  @Public()
   public login(@Body() loginDto: LoginDto) {
     return this.authService.authenticate(loginDto);
   }
