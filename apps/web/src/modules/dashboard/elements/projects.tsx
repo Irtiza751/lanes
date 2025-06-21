01906f0cd79c7695588764ced8a0c085786cdbfb
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-06-22 00:58:56 +0500

    feat: restructure Projects component layout for improved readability

--- apps/web/src/modules/dashboard/elements/projects.tsx
+++ apps/web/src/modules/dashboard/elements/projects.tsx
@@ -31,26 +31,29 @@ export function Projects() {
   ]
 
   return (
-    <Table>
-      <TableCaption>List of all you projects.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">ID</TableHead>
-          <TableHead className="w-[100px]">Title</TableHead>
-          <TableHead>Description</TableHead>
-          <TableHead className="text-right">Status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {projects.map(project => (
+    <div>
+      <h2 className='text-lg font-semibold'>Projects</h2>
+      <Table>
+        <TableCaption>List of all you projects.</TableCaption>
+        <TableHeader>
           <TableRow>
-            <TableCell className="font-medium">{project.id}</TableCell>
-            <TableCell className="font-medium">{project.name}</TableCell>
-            <TableCell>{project.description}</TableCell>
-            <TableCell className="text-right">{project.status}</TableCell>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead className="w-[100px]">Title</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead className="text-right">Status</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {projects.map(project => (
+            <TableRow>
+              <TableCell className="font-medium">{project.id}</TableCell>
+              <TableCell className="font-medium">{project.name}</TableCell>
+              <TableCell>{project.description}</TableCell>
+              <TableCell className="text-right">{project.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
