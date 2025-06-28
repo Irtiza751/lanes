a3e121677c41666a4c7d4c47c7a15f5ea9042070
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-06-28 21:00:03 +0500

    feat: abstract the loader logic into the auth guard

--- apps/web/src/modules/routing.module.tsx
+++ apps/web/src/modules/routing.module.tsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import Login from './login'
-import Home from './home'
 import HomeLayout from '@/layouts/home.layout'
 import AuthLayout from '@/layouts/auth.layout'
-import Protected from '@/shared/components/protected'
 import { dashboardRoutes } from './dashboard/dashboard.routing'
 import { loginRoutes } from './login/login-routring.module'
 import { homeRoutes } from './home/home-routring.module'
@@ -11,20 +8,6 @@ import { homeRoutes } from './home/home-routring.module'
 const router = createBrowserRouter([
   {
     path: '',
-    Component: HomeLayout,
-    children: [
-      {
-        index: true,
-        element: (
-          <Protected>
-            <Home />
-          </Protected>
-        ),
-      },
-    ],
-  },
-  {
-    path: 'home',
     Component: HomeLayout,
     children: [
       homeRoutes
