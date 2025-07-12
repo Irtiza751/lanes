490722c9c937c76831c4ac126ae2b7dbe45dea11
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-07-12 18:58:40 +0500

    feat: add CreateTaskProvider to TasksModule providers

--- apps/api/src/tasks/tasks.module.ts
+++ apps/api/src/tasks/tasks.module.ts
@@ -4,10 +4,11 @@ import { TasksController } from './tasks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { UsersModule } from 'src/users/users.module';
+import { CreateTaskProvider } from './providers/create-task.provider';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, CreateTaskProvider],
   imports: [TypeOrmModule.forFeature([Task]), UsersModule]
 })
 export class TasksModule {}
