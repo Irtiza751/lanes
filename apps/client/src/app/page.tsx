8a71944c98b3e5896eb239f7a2e06b0f7cd384e3
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-07-26 21:09:15 +0500

    fix font family issue

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="container mx-auto px-10">
-      <h1 className="text-3xl font-bold">Hello</h1>
+      <h1 className="text-3xl font-bold">Hello, world</h1>
       <Button>Creat new task</Button>
     </div>
   );
