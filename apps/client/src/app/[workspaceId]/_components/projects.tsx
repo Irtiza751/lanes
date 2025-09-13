4586941d4a87e9756c17823e9831a672de36e739
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-09-13 17:25:09 +0500

    comment un used code

--- apps/client/src/app/[workspaceId]/_components/projects.tsx
+++ apps/client/src/app/[workspaceId]/_components/projects.tsx
@@ -5,18 +5,19 @@ import { AuthService } from "@/lib/auth-service";
 import { useQuery } from "@tanstack/react-query";
 
 export function ProjectsPage() {
-  const { data, error } = useQuery({
-    queryKey: ["whoami"],
-    queryFn: async () => AuthService.whoami(),
-    retry: false,
-  });
+  // const { data, error } = useQuery({
+  //   queryKey: ["whoami"],
+  //   queryFn: async () => AuthService.whoami(),
+  //   retry: false,
+  // });
 
-  if (error) return <div>Error loading user data</div>;
+  // if (error) return <div>Error loading user data</div>;
 
   return (
     <Card>
       <CardContent>
-        <pre>{JSON.stringify(data?.data.user, null, 2)}</pre>
+        <p>Project page</p>
+        {/* <pre>{JSON.stringify(data?.data.user, null, 2)}</pre> */}
       </CardContent>
     </Card>
   );
